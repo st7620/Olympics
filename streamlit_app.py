765a560ac3571c2d96d6aec8a7fa58c59c6d09da
Sonia Tan <soniatan127@gmail.com>
2024-07-12 11:34:51 -0400

    Added acf and pacf plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import altair as alt
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_pacf
 
 ## Page Config
 st.set_page_config(
@@ -13,6 +15,8 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
     )
+
+alt.themes.enable("dark")
 
 ## Load data
 df_final = pd.read_csv('Datasets/final.csv')
@@ -73,6 +77,20 @@ def bar_chart_predictions(input_df):
     
     return chart
 
+# ACF and PACF Plots:
+def acf_pacf_plot(input_df, input_country):
+    # Filter the data
+    df_country = input_df.loc[input_df['Country'] == input_country]
+    reduced_df = df_country[['Year', 'total']]
+
+    fig, ax = plt.subplots(2,1, figsize=(12,8))
+    plot_acf(reduced_df)
+    plot_pacf(reduced_df)
+    ax[0].set_title('Autocorrelation Plot')
+    ax[1].set_title('Partial Autocorrelation Plot')
+    plt.tight_layout()
+    return fig
+
 ## Dashboard Main Panel:
 col = st.columns((2,3,2), gap='medium')
 
@@ -81,6 +99,10 @@ with col[0]:
 
     ts = make_time_series(df_final, selected_country)
     st.plotly_chart(ts, use_container_width=True)
+
+    st.markdown('#### ACF and PACF Plots')
+    acf = acf_pacf_plot(df_final, selected_country)
+    st.pyplot(acf)
 
 with col[1]:
     st.markdown('#### Medal Predictions')
@@ -93,4 +115,9 @@ with col[2]:
 
     bc = bar_chart(df_results)
     st.pyplot(bc, use_container_width=True)
+
+    with st.expander('About', expanded=True):
+        st.write('''
+            The first graph shows the total number of medals won by the selected country over time. You can select a country from the sidebar to see the data for that country. 
+            ''')
 
